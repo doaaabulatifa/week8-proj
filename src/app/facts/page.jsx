9a09d17eca941915e747848ddf7a7e8b0fdcfd0c
doaaabulatifa <doaalatifa1@gmail.com>
2024-05-15 21:38:29 +0100

    test

--- src/app/facts/page.jsx
+++ src/app/facts/page.jsx
@@ -14,10 +14,12 @@ export default async function Facts() {
         {facts.map((fact) => (
           <Link href={`/facts/${fact.fact_id}`} key={fact.fact_id} >
             <div className={styles.fact}>
-              <h3 className={styles.title}>Fact Title: {fact.title}</h3>
-              <p className={styles.content}>Fact Content: {fact.content}</p>
-              <p className={styles.link}>
-                Fact Source: {fact.link}
+              <h2>Fact Title:</h2>
+              <h3 className={styles.title}> {fact.title}</h3>
+              <h2>Fact Content:</h2>
+              <p className={styles.content}> {fact.content}</p>
+              <h2>Fact Source:</h2>
+              <p className={styles.link}> {fact.link}
               </p>
             </div>
           </Link>
